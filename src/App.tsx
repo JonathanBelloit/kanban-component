9789e2e848ce85934599f3df774481ab7710d6e3
Jonathan Belloit <Jonathan@topdogwebdev.com>
2024-07-13 11:41:41 -0400

    added Login to root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import DashBoard from "./components/DashBoard"
 import TopBar from "./components/TopBar"
-// import Login from "./components/auth/Login"
+import Login from "./components/auth/Login"
+import useAuth from "./hooks/useAuth"
 
 function App() {
+  const { user, loading } = useAuth();
   return (
     <>
-      <TopBar />
-      <DashBoard />
+      { loading && <div>Loading...</div> }
+      { !user && <Login /> }
+      { user && (
+        <>
+          <TopBar />
+          <DashBoard />
+        </>
+      )}
     </>
   )
 }
